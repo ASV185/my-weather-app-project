ceec91f4f71df75773ffc7e7de702b6fe4b24491
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-01-23 19:03:47 -0700

    change time JS

--- src/index.js
+++ src/index.js
@@ -25,19 +25,18 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayWeatherCondition(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].description;
-  dateElement.innerHTML = formatDate(response.data.dt*1000);
+  let temperatureElement= document.querySelector("#temperature");
+  let cityElement= document.querySelector("#city");
+  let descriptionElement= document.querySelector("#description");
+  let humidityElement= document.querySelector("#humidity");
+  let windElement= document.querySelector("#wind");
+  let dateElement= document.querySelector("#date");
+  temperatureElement.innerHTML= Math.round (response.data.main.temp);
+  cityElement.innerHTML= response.data.name;
+  descriptionElement.innerHTML= response.data.weather[0].description;
+  humidityElement.innerHTML= response.data.main.humidity;
+  windElement.innerHTML= Math.round(response.data.wind.speed);
+  dateElement.innerHTML= formatDate(response.data.dt*1000);
 }
 
 function searchCity(city) {
