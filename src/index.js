62eb83151243d848703e37f4c4e940c1ae55d0e3
ASV185 <74805696+ASV185@users.noreply.github.com>
2021-01-24 20:10:51 -0700

    change JS remove active

--- src/index.js
+++ src/index.js
@@ -69,15 +69,11 @@ function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let fahrenheitTemperature= (celsiusTemperature*9)/5+32;
   let temperatureElement= document.querySelector ("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
   temperatureElement.innerHTML= Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.add("active");
   let temperatureElement= document.querySelector ("#temperature");
   temperatureElement.innerHTML= Math.round(celsiusTemperature);
 }
